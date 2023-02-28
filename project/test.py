7bef36fe8e2b058609e11bbdc6bea6a29b9633ea
Feela <filipplachycz@gmail.com>
2023-02-28 11:56:07 +0100

    jo

--- project/test.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-args = ['https://www.youtube.com/watch?v=8tlzx5Wuy8s','--output','05mechanika.mp4','--start','2023-02-20T14:00','--end','2023-02-20T15:50']
-print(args)
-subprocess.run(['python','yt_ddl.py'] + args)
-
-
- 
